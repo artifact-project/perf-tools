940a53f11f06b39f4ccc5c41d834453297a2e9ae
Igor Frolov <imfrolov@gmail.com>
2019-04-17 22:58:27 +0300

    fix: underline before label

--- keeper/analytics/utils.ts
+++ keeper/analytics/utils.ts
@@ -4,14 +4,14 @@ export const globalThis = Function('return this')() as Window;
 
 export type AnalyticsOptions = {
 	prefix?: string;
-	rename: (group: string, label: string) => string;
+	rename: (group: string, label?: string) => string;
 	sendZeroValues?: boolean;
 	useTabName?: (location: Location) => string;
 }
 
 const baseAnalyticsOptions: AnalyticsOptions = {
 	prefix: '',
-	rename: (group, label = '') => `${group}${label}`,
+	rename: (group, label = '') => `${group}${label && `_${label}`}`,
 	sendZeroValues: false,
 	useTabName: ({pathname}: Location) => (pathname === '/' ? 'index' : pathname)
 		.replace(/[\/\.]+/g, '-')
