141d34a929c900ca48e65ccd6915c8c59a7f0798
RubaXa <ibnRubaXa@gmail.com>
2019-07-24 09:58:55 +0300

    v1.6.0: + filter for paint extension

--- keeper/ext/paint/README.md
+++ keeper/ext/paint/README.md
@@ -30,4 +30,19 @@ import { system } from '@perf-tools/keeper';
 import { paintTimings } from '@perf-tools/keeper/ext/paint';
 
 paintTimings(system);
+
+// With options
+paintTimings(system, {
+	filter: (duration: number, entry: PaintTimingsOptions) => {
+		if (duration === 0) {                  // 0ms
+			return false;  // skip metric
+		} else if (duration < 30 * 1000) {     // < 30sec
+			return true; // normal metric value
+		} else if (duration < 5 * 60 * 1000) { // < 5min
+			return `${entry.name}_slow`; // new metric name
+		} else {                               // > 5min
+			return `${entry.name}_terribly`;
+		}
+	},
+});
 ```
--- keeper/ext/paint/index.ts
+++ keeper/ext/paint/index.ts
@@ -2,12 +2,20 @@ import { PerfKeeper } from '../../src/keeper/keeper';
 import { domReady, createTimingsGroup, performance } from '../utils';
 
 export type PaintTimingsOptions = {
+	filter: (duration: number, entry: PerformanceEntry) => boolean | string;
 }
 
 export const defaultPaintTimingsOptions: PaintTimingsOptions = {
+	filter: (duration, entry) => duration > 60 * 1000 ? `${entry.name}_slow` : duration > 0,
 };
 
-export function paintTimings(keeper: PerfKeeper, _: PaintTimingsOptions = defaultPaintTimingsOptions) {
+export function paintTimings(
+	keeper: PerfKeeper,
+	options?: PaintTimingsOptions,
+) {
+	const {
+		filter = defaultPaintTimingsOptions.filter,
+	} = options || defaultPaintTimingsOptions;
 	const [set, send] = createTimingsGroup('pk-paint', keeper);
 
 	domReady(function check() {
@@ -17,17 +25,23 @@ export function paintTimings(keeper: PerfKeeper, _: PaintTimingsOptions = defaul
 			if (entries.length > 1) {
 				let duration = 0;
 				let startTime = 0;
+				let name: string;
+				let f: boolean | string;
 
 				entries
 					.sort((a, b) => a.startTime - b.startTime)
 					.forEach(entry => {
 						startTime = entry.startTime;
-						duration = Math.max(duration, startTime);
-						set(entry.name, 0, startTime);
+						name = entry.name;
+						f = filter(startTime, entry);
+						if (f) {
+							duration = Math.max(duration, startTime);
+							set(f === true ? name : f, 0, startTime);
+						}
 					})
 				;
 
-				send(null, 0, duration, true);
+				duration && send(null, 0, duration, true);
 			} else {
 				setTimeout(check, 250);
 			}
