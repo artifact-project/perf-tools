c383c49fcaac1fc541b5efa4fee77a8608491037
Lebedev Konstantin <ibnrubaxa@gmail.com>
2023-08-10 16:32:16 +0300

    Починил initPerf

--- keeper/ext/index.ts
+++ keeper/ext/index.ts
@@ -3,7 +3,6 @@ import './navigation';
 import './connection';
 import './device';
 import './navigation';
-import './performance';
 import {useWebVitals} from './web-vitals';
 
 useWebVitals();
--- keeper/ext/web-vitals/index.ts
+++ keeper/ext/web-vitals/index.ts
@@ -1,21 +1,33 @@
 
 import { Metric, onCLS, onFCP, onLCP, onFID, onINP, onTTFB } from 'web-vitals';
 import { send } from '../../util/system';
+import { initPerf } from '../performance';
 
 export const useWebVitals = () => {
+	const sendMetric = (name: string, {value, rating}: Metric) => {
+		send(`pk-${name}`, 0, value, {
+			value: [0, value],
+			[`score_${rating}`]: [0, value],
+		});
+	};
 	const use = <TMetric extends Metric>(name: string, onMetric: (fn: (data: TMetric) => void) => void) => {
-		onMetric(({value, rating}) => {
-			send(`pk-${name}`, 0, value, {
-				value: [0, value],
-				[`score_${rating}`]: [0, value],
-			});
+		onMetric((data) => {
+			sendMetric(name, data);
 		});
 	};
 
-	use('paint', onFCP);
+	//
+	// Подписываемся на сбор метрик
+	//
+	onFCP((data) => {
+		initPerf(data.value);
+		sendMetric('paint', data);
+	});
+
 	use('cls', onCLS);
 	use('lcp', onLCP);
 	use('fid', onFID);
 	use('inp', onINP);
 	use('ttfb', onTTFB);
+
 };
